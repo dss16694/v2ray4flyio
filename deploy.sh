351b0ace7ef800b2c503b9582d78881872fd53fe
dss16694 <dss16694@163.com>
2022-01-14 08:49:55 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,18 +14,7 @@ if [ -z "${APP_NAME}" ]; then
     printf '\e[31mPlease set APP_NAME first.\n\e[0m' && exit 1
 fi
 
-if [ "$(flyctl info --app "${APP_NAME}" | grep -o "Could not resolve App")" = "Error Could not resolve App" ]; then
-    printf '\e[33mCould not resolve app. The app name is ${APP_NAME}.Next, create the App.\n\e[0m'
-    flyctl apps create "${APP_NAME}"
-
-    if [ "$(flyctl info --app "${APP_NAME}" | grep -o "Could not resolve App")" = "Error Could not resolve App" ]; then
-        printf '\e[31mCreate app failed.\n\e[0m' && exit 1
-    else
-        printf '\e[32mCreate app success.\n\e[0m'
-    fi
-else
-    printf '\e[33mThe app has been created.\n\e[0m'
-fi
+flyctl apps create "${APP_NAME}"
 
 printf '\e[33mNext, create app config file - fly.toml.\n\e[0m'
 cat <<EOF >./fly.toml
